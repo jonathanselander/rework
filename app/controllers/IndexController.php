83814798be33e6e0723761eb568825a09872d80e
Jonathan Selander <selander.jonathan@gmail.com>
2012-08-29 02:22:10 +0200

    update documentation

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -12,7 +12,6 @@ class IndexController extends Rework_Controller
     /**
      * Receive GET
      * Call a method before the action runs
-     *  DashboardController might send a routing exception
      * 
      * @before AppHelper::requiresLogin
      */
